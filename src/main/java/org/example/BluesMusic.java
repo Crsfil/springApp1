d5b34471247bd7fc33d5b3bbe69184b2f4f8ffdf
Crsfil <strizhovhotmail@gmail.com>
2024-12-10 06:38:41 +0300

    firstCommit

---
+++ src/main/java/org/example/BluesMusic.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class BluesMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Buckethead - Three lee s gone";
+    }
+}
---
+++ src/main/java/org/example/TestSpring.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+
+        Music music = context.getBean("musicBean", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
+        context.close();
+    }
+}
